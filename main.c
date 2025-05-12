b2a3d087acc6be4b3087c6f1ed40143c9741293c
aSumo-1xts <1xtelescope@gmail.com>
2025-05-12 11:13:57 +0900

    add: PIC16F15313を追加

---
+++ main.c
@@ -0,0 +1,12 @@
+/**
+ * @file main.c
+ * @author asumo
+ * @date 2025-05-12
+ * @brief Main function
+ */
+int main(){
+
+    // Add your code here and press Ctrl + Shift + B to build
+
+    return 0;
+}
